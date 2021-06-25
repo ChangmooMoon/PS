0fb7c5664b719a629a62f1ad6e82c59ceffe0d8f
changmoo moon <public.stevemoon@gmail.com>
2021-06-26 01:36:44 +0900

    bfs

--- boj/12886.cpp
+++ boj/12886.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <queue>
+#include <tuple>
 #define pii pair<int, int>
 using namespace std;
 
 int a, b, c, sum;
 bool check[1501][1501];
+
+int bfs() {
+    int ret = 0;
+    queue<pii> q;
+    q.push({a, b});
+    check[a][b] = true;
+
+    while (!q.empty()) {
+        int x, y, z;
+        tie(x, y) = q.front();
+        q.pop();
+        z = sum - x - y;
+        if (x == y && y == z) {
+            ret = 1;
+            break;
+        }
+
+        int list[3] = {x, y, z};
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (list[i] < list[j]) {  // X < Y
+                    int nList[3] = {x, y, z};
+                    nList[i] += list[i];
+                    nList[j] -= list[i];
+
+                    int nx = nList[0];
+                    int ny = nList[1];
+                    if (check[nx][ny]) continue;
+                    q.push({nx, ny});
+                    check[nx][ny] = true;
+                }
+            }
+        }
+    }
+
+    return ret;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -18,26 +56,6 @@ int main() {
         return 0;
     }
 
-    queue<pii> q;
-    q.push({a, b});
-
-    while (!q.empty()) {
-        int x, y;
-        tie(x, y) = q.front();
-        q.pop();
-        check[x][y] = true;
-
-        int list[3] = {x, y, sum - x - y};
-        for (int i = 0; i < 3; ++i) {      // choose the smallest
-            for (int j = 0; j < 3; ++j) {  // target
-                if (list[i] < list[j]) {
-                    int nList[3] = {x, y, sum - x - y};
-                    nList[i] += list[i];
-                    nList[j] -= list[i];
-                                }
-            }
-        }
-    }
-
+    cout << bfs();
     return 0;
 }
