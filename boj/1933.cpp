d6740616f16d2c4374c48aa1f241c14f001f3453
changmoo moon <public.stevemoon@gmail.com>
2021-08-03 00:07:26 +0900

    recursion

--- boj/1933.cpp
+++ boj/1933.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-#include <map>
+#include <tuple>
 #include <vector>
 using namespace std;
+
+int n;
+tuple<int, int, int> a[100001];
+vector<pair<int, int>> v;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int l, h, r;
+        cin >> l >> h >> r;
+        v.push_back({l, h});
+        v.push_back({r, -h});
+    }
+
+    sort()
 
     return 0;
 }
