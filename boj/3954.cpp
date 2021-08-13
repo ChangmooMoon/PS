504bd4131628ed6c99e6251edae819603117fded
changmoo moon <public.stevemoon@gmail.com>
2021-08-14 01:04:40 +0900

    union-find

--- boj/3954.cpp
+++ boj/3954.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
-#include <stack>
 #include <string>
 #include <vector>
 using namespace std;
+#define endl '\n'
+#define FASTIO cin.tie(nullptr)->sync_with_stdio(false)
 
-int t, mSize, cLen, iLen;  // 배열크기10만, 코드길이4096, 입력길이4096
-string program, input;     // 프로그램, 입력
-int p;                     // 포인터
-stack<int> st;             // 괄호 시작인덱스
-
-string solve() {
-    for (int i = 0; i < cLen++ i) {
-        char cmd = program[i];
-
-        switch (cmd) {
-            case '-':
-                break;
-            case '+':
-                break;
-            case '<':
-                break;
-            case '>':
-                break;
-            case '[':
-                break;
-            case ']':
-                break;
-            case '.':
-                break;
-            case ',':
-                break;
-            default:
-                break;
-        }
-    }
-}
+int t;           // 20
+int sm, sc, si;  // 메모리크기10만, 코드크기2^12, 입력크기2^12
+string program, input;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
+    FASTIO;
     cin >> t;
     while (t--) {
-        cin >> mSize >> cLen >> iLen >> program >> input;
-        p = 0;
-        while (st.empty()) st.pop();
-
-        cout << solve() << '\n';
+        cin >> sm >> sc >> si >> program >> input;
     }
 
     return 0;
--- library/unionFind.cpp
+++ library/unionFind.cpp
@@ -1,13 +1,42 @@
-int find(int a) {
-    if (a == parent[a]) return a;
-    return parent[a] = find(parent[a]);
+#include <iostream>
+#define union _union
+using namespace std;
+
+int parent[11];
+
+int find(int x) {  // 부모찾기
+    if (parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
 }
 
-void union(int a, int b) {
+void union(int a, int b) {  // 각 부모 노드를 합친다
     a = find(a);
     b = find(b);
     if (a < b)
         parent[b] = a;
     else
         parent[a] = b;
+}
+
+bool sameParent(int a, int b) {  // 같은 부모 노드인지 확인한다
+    a = find(a);
+    b = find(b);
+    if (a == b) return true;
+    return false;
+}
+
+int main() {
+    for (int i = 0; i < 11; ++i) {
+        parent[i] = i;
+    }
+
+    union(1, 2);
+    union(2, 3);
+    union(3, 4);
+    union(5, 6);
+    union(6, 7);
+    union(7, 8);
+    cout << sameParent(1, 5) << endl;
+    union(1, 5);
+    cout << sameParent(1, 5) << endl;
 }
