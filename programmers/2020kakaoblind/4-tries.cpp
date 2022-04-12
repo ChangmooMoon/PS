d9d35009d4d63c94e7c986636c8d484ca27057c7
changmoo moon <public.stevemoon@gmail.com>
2022-04-12 17:55:00 +0900

    tries template

--- programmers/2020kakaoblind/4-tries.cpp
+++ programmers/2020kakaoblind/4-tries.cpp
@@ -1,77 +1,34 @@
-#include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <string>
-#include <vector>
-
 using namespace std;
 
-const int ALPHABET_SIZE = 26;
-
-struct TrieNode {
-    vector<TrieNode*> children;
-    int count;
-
-    TrieNode() : children(ALPHABET_SIZE), count(0) {
-    }
-
-    void insert(string& s) {
-        TrieNode* curr = this;
-        for (char& ch : s) {
-            curr->count++;
-            int next = ch - 'a';
-            if (curr->children[next] == nullptr) {
-                curr->children[next] = new TrieNode();
-            }
-
-            curr = curr->children[next];
-        }
-
-        curr->count++;
-    }
-
-    int search(string& s) {
-        TrieNode* curr = this;
-        int count = 0;
-        for (char& ch : s) {
-            if (ch == '?') {
-                count = curr->count;
-                break;
-            }
-
-            curr = curr->children[ch - 'a'];
-            if (curr == nullptr) {
-                break;
-            }
-        }
-
-        return count;
+class TrieNode {
+  public:
+    bool end;
+    TrieNode *child[27];
+    TrieNode() {
+        end = false;
+        memset(child, NULL, sizeof(child));
     }
 };
 
-vector<int> solution(vector<string> words, vector<string> queries) {
-    vector<int> answer;
+TrieNode *root = new TrieNode(); /* Root Creation */
 
-    TrieNode TrieRoot[10001];
-    TrieNode ReverseTrieRoot[10001];
-
-    for (string& w : words) {
-        int len = w.length();
-        TrieRoot[len - 1].insert(w);
-        reverse(w.begin(), w.end());
-        ReverseTrieRoot[len - 1].insert(w);
+void insert(string word) { /* Insert word into the trie */
+    TrieNode *node = root;
+    for (char c : word) {
+        if (!node->child[c - 'a']) { node->child[c - 'a'] = new TrieNode(); }
+        node = node->child[c - 'a'];
     }
+    node->end = true;
+}
 
-    for (string& q : queries) {
-        int ans;
-        int len = q.length();
-        if (q[0] == '?') {
-            reverse(q.begin(), q.end());
-            ans = ReverseTrieRoot[len - 1].search(q);
-        } else {
-            ans = TrieRoot[len - 1].search(q);
-        }
-        answer.push_back(ans);
+bool search(string word) { /* Search word into the trie */
+    TrieNode *node = root;
+    for (char c : word) {
+        if (!node->child[c - 'a']) { return false; }
+        node = node->child[c - 'a'];
     }
-
-    return answer;
+    return node->end;
 }
