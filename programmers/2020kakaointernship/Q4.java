6a6d2e4d245f0e396ac5390629fe974dea7994ab
changmoo moon <public.stevemoon@gmail.com>
2022-04-22 20:32:25 +0900

    bfs, simulation

--- programmers/2020kakaointernship/Q4.java
+++ programmers/2020kakaointernship/Q4.java
@@ -1,85 +1,57 @@
 import java.util.*;
 
 class Solution {
-    int n;
-    int[][] map;
-    boolean[][] visit;
+    final int INF = 0x3f3f3f3f;
 
-    int[] dx = { -1, 0, 1, 0 };
-    int[] dy = { 0, 1, 0, -1 };
-
-    int cost = Integer.MAX_VALUE;
+    int[] dr = new int[] { -1, 0, 1, 0 };
+    int[] dc = new int[] { 0, 1, 0, -1 };
 
     public int solution(int[][] board) {
-        int answer = 0;
-        n = board.length;
+        int N = board.length;
 
-        map = board;
-        visit = new boolean[n][n];
-
-        bfs(0, 0, -1, 0);
-
-        answer = cost;
-
-        return answer;
-    }
-
-    void bfs(int x, int y, int dir, int price) {
-
-        Queue<Road> q = new LinkedList();
-        q.add(new Road(x, y, dir, price));
-        visit[x][y] = true;
-
-        while (!q.isEmpty()) {
-            Road road = q.remove();
-
-            int qx = road.x;
-            int qy = road.y;
-            int qDir = road.dir;
-            int qPrice = road.cost;
-
-            if (qx == n - 1 && qy == n - 1) {
-                cost = Math.min(cost, qPrice);
-            }
-
-            for (int i = 0; i < dx.length; i++) {
-                int nx = qx + dx[i];
-                int ny = qy + dy[i];
-                int nDir = i;
-                int nPrice = qPrice;
-
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n || map[nx][ny] == 1) {
-                    continue;
+        int[][][] d = new int[25][25][4]; // r, c, dir;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    if (i == 0 && j == 0)
+                        d[i][j][k] = 0;
+                    else
+                        d[i][j][k] = INF;
                 }
-
-                if (qDir == -1) {
-                    nPrice += 100;
-                } else if (qDir == nDir) {
-                    nPrice += 100;
-                } else {
-                    nPrice += 600;
-                }
-
-                if (!visit[nx][ny] || map[nx][ny] >= nPrice) {
-                    visit[nx][ny] = true;
-                    map[nx][ny] = nPrice;
-                    q.add(new Road(nx, ny, nDir, nPrice));
-                }
-
             }
         }
 
-    }
+        Queue<int[]> q = new LinkedList<>(); // r, c, dir, cost
+        q.add(new int[] { 0, 0, -1, 0 });
 
-}
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int r = now[0], c = now[1], dir = now[2], cost = now[3];
 
-class Road {
-    int x, y, dir, cost;
+            for (int nDir = 0; nDir < 4; ++nDir) {
+                int nr = r + dr[nDir];
+                int nc = c + dc[nDir];
+                int nCost = cost + 100;
 
-    Road(int x, int y, int dir, int cost) {
-        this.x = x;
-        this.y = y;
-        this.dir = dir;
-        this.cost = cost;
+                if (0 > nr || 0 > nc || N <= nr || N <= nc || board[nr][nc] == 1)
+                    continue;
+
+                if ((dir == 0 || dir == 2) && (nDir == 1 || nDir == 3))
+                    nCost += 500;
+                if ((dir == 1 || dir == 3) && (nDir == 0 || nDir == 2))
+                    nCost += 500;
+
+                if (nCost < d[nr][nc][nDir]) {
+                    d[nr][nc][nDir] = nCost;
+                    q.add(new int[] { nr, nc, nDir, nCost });
+                }
+            }
+        }
+
+        int ans = INF;
+        for (int k = 0; k < 4; ++k) {
+            ans = Math.min(ans, d[N - 1][N - 1][k]);
+        }
+        return ans;
     }
 }
