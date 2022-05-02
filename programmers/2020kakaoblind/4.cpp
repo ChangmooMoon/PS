17008c71b3cd57166951a03a33f76cdc2b190ad3
changmoo moon <public.stevemoon@gmail.com>
2022-05-02 19:04:49 +0900

    binary search

--- programmers/2020kakaoblind/4.cpp
+++ programmers/2020kakaoblind/4.cpp
@@ -3,55 +3,50 @@
 #include <vector>
 using namespace std;
 
-bool cmp(const string& a, const string& b) {
+bool cmp(string a, string b) {
     if (a.length() == b.length()) return a < b;
     return a.length() < b.length();
 }
 
-// 찾고자 하는 키워드queires 10만, 모든 단어 담긴 배열 words 10만
 vector<int> solution(vector<string> words, vector<string> queries) {
-    int N = queries.size();
-    vector<int> answer(N);
+    vector<int> ans;
 
     vector<string> rwords = words;
     sort(words.begin(), words.end(), cmp);
-    for (string& s : rwords) {
+    for (string &s : rwords) {
         reverse(s.begin(), s.end());
     }
     sort(rwords.begin(), rwords.end(), cmp);
 
-    // words {"frodo", "front", "frost", "frozen", "frame", "kakao"};
-    // queries {"pro?", "????o", "fr???", "fro??", "fro???"}
+    for (string q : queries) {
+        if (q[0] != '?') {
+            string l = q, r = q;
 
-    for (int i = 0; i < queries.size(); ++i) {  // ? 63, a 97 z 122
-        if (queries[i][0] != '?') {             // ?로 끝남
-            string lquery = queries[i], rquery = queries[i];
-            for (char& ch : lquery) {
+            for (char &ch : l) {
                 if (ch == '?') ch = 'a';
-            }  // fraaa
-            for (char& ch : rquery) {
+            }
+            for (char &ch : r) {
                 if (ch == '?') ch = 'z';
-            }  // frzzz
+            }
 
-            // 오름차순 정렬에서 특정범위 속하는 숫자가 몇개 있는지, k상한 - k하한 = k갯수
-            auto lIt = lower_bound(words.begin(), words.end(), lquery, cmp);  // fraaa 하한위치
-            auto rIt = upper_bound(words.begin(), words.end(), rquery, cmp);  // frzzz 상한위치
-            answer[i] = rIt - lIt;
-        } else {  // ?로 시작함
-            reverse(queries[i].begin(), queries[i].end());
-            string lquery = queries[i], rquery = queries[i];
-            for (char& ch : lquery) {
+            auto rIt = upper_bound(words.begin(), words.end(), r, cmp);
+            auto lIt = lower_bound(words.begin(), words.end(), l, cmp);
+            ans.push_back(rIt - lIt);
+        } else {
+            reverse(q.begin(), q.end());
+            string l = q, r = q;
+
+            for (char &ch : l) {
                 if (ch == '?') ch = 'a';
-            }  // aaaao -> oaaaa
-            for (char& ch : rquery) {
+            }
+            for (char &ch : r) {
                 if (ch == '?') ch = 'z';
-            }  // zzzzo -> ozzzz
-
-            auto lIt = lower_bound(rwords.begin(), rwords.end(), lquery, cmp);
-            auto rIt = upper_bound(rwords.begin(), rwords.end(), rquery, cmp);
-            answer[i] = rIt - lIt;
+            }
+            auto rIt = upper_bound(rwords.begin(), rwords.end(), r, cmp);
+            auto lIt = lower_bound(rwords.begin(), rwords.end(), l, cmp);
+            ans.push_back(rIt - lIt);
         }
     }
 
-    return answer;
+    return ans;
 }
