b2c907221d2c22630ef02a400cc030df1d31a4a7
changmoo moon <public.stevemoon@gmail.com>
2022-05-20 15:13:35 +0900

    bfs

--- boj/2178.cpp
+++ boj/2178.cpp
@@ -1,45 +1,47 @@
-#include <iostream>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define FASTIO cin.tie(nullptr)->sync_with_stdio(false)
 
-int dy[]{1, -1, 0, 0};
-int dx[]{0, 0, 1, -1};
-int n, m;         // 도착지
-int d[101][101];  // 거리
-int a[101][101];  // 맵
+int dr[] = {-1, 0, 1, 0};
+int dc[] = {0, 1, 0, -1};
+
+int n, m, a[101][101], d[101][101];
+
+int bfs(int sr, int sc, int er, int ec) {
+    memset(d, -1, sizeof(d));
+
+    queue<pair<int, int>> q;
+    q.push({sr, sc});
+    d[sr][sc] = 1;
+
+    while (!q.empty()) {
+        const auto [r, c] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; ++i) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (0 > nr || 0 > nc || n <= nr || m <= nc) continue;
+            if (!a[nr][nc] || d[nr][nc] != -1) continue;
+
+            q.push({nr, nc});
+            d[nr][nc] = d[r][c] + 1;
+            if (nr == er && nc == ec) return d[nr][nc];
+        }
+    }
+
+    return -1;
+}
 
 int main() {
     cin >> n >> m;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
             scanf("%1d", &a[i][j]);
         }
     }
 
-    queue<pair<int, int>> q;
-    q.push({1, 1});
-    d[1][1] = 1;
-    while (!q.empty()) {
-        int y = q.front().first;
-        int x = q.front().second;
-        q.pop();
-
-        if (y == n && x == m) {
-            cout << d[y][x];
-            break;
-        }
-
-        for (int i = 0; i < 4; ++i) {
-            int ny = y + dy[i];
-            int = x + dx[i];
-            if (1 <= ny && ny <= n && 1 <= && <= m && !d[ny][] && a[ny][]) {
-                d[ny][] = d[y][x] + 1;
-                q.push({
-                    ny,
-                });
-            }
-        }
-    };
+    cout << bfs(0, 0, n - 1, m - 1);
     return 0;
 }
