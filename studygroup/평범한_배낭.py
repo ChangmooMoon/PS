1f2e5757ae5f9b82e34e7ea6e0a513d5ca81cd29
changmoo <public.ellerymoon@gmail.com>
2023-03-10 17:14:33 +0900

    knapsack

--- studygroup/평범한_배낭.py
+++ studygroup/평범한_배낭.py
@@ -2,21 +2,21 @@ import sys
 si = sys.stdin.readline
 
 N, K = map(int, si().split())
-d = [[0] * 101 for _ in range(100001)]
-W, V = [0] * 101, [0] * 101
-
-for i in range(1, N + 1):
-    W[i], V[i] = map(int, si().split())
+W, V = [0], [0]
+d = [[0] * (K + 1) for _ in range(N + 1)]
+for _ in range(N):
+    w, v = map(int, si().split())
+    W.append(w)
+    V.append(v)
 
 for i in range(1, N + 1):
     for j in range(1, K + 1):
-        d[i][j] = d[i - 1][j]
-        if j - W[i] >= 0:
-            d[i][j] = max(d[i][j], d[i - 1][j - W[i]] + V[i])
+        if j < W[i]:
+            d[i][j] = d[i - 1][j]
+        else:
+            d[i][j] = max(d[i - 1][j], d[i - 1][j - W[i]] + V[i])
 
 print(d[N][K])
-
-
 '''
 1. 문제 읽기
 2. 논리적 순서
@@ -25,17 +25,14 @@ print(d[N][K])
 5. 구현
 6. 엣지케이스
 
-1. N개의 물건, 무게w, 가치v를 가진다. 최대 k 만큼의 무게를 넣을 수 있음
-넣을 수 있는 가치의 최대값
+1. N 100개의 물건, 무게 w 10만, 가치v 1000, 가방 최대k10만
+배낭에 넣을 수 있는 물건들의 가치의 최댓값
+그리디로 못푸나?
+d[i][j] = i번째 물건을 넣었을 때 무게는 j이고, 가방 물건의 가치
 
-N 100, K 10만, w 10만, v 1000
+d[0][0] = 0
 
-2. 
-- 모든 조합 0000~1111 2^N
-- 모든 무게에 대해서 각각의 물건을 넣거나 빼는 경우
-
-d[i][j] = 물건 i를 담았을 때 무게는 j일 때의 최대 가치
-d[i][j] = max(d[i - 1][j], d[i - 1][j - w[i]] + v[i])
-
+d[i][j]  = d[i - 1][j], j < W[i]
+d[i][j] = d[i - 1][j - W[i]] + V[i], j >= W[i]
 
 '''
